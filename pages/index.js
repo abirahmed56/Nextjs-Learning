e7efbeef52e26a853fa318c60f183e2e94065396
abirahmed56 <abirahmedsohan6@gmail.com>
2023-02-15 17:08:36 +0600

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,40 @@
+import Link from "next/link";
 const index = ()=>{
   return(
     <>
-      <h1>
-          hello world
-      </h1>
+      <nav>
+        <ul className="menu-bar">
+          <Link href="blogs/Home">
+             Home
+          </Link>
+          <Link href="blogs/about">
+            About
+          </Link>
+          <Link href="blogs/contact">
+             Contact
+          </Link>
+          <Link href="blogs">
+             Blog
+          </Link>
+          <Link href="blogs/product">
+            Product
+          </Link>
+        </ul>
+      </nav>
+      <handler className = "handler">
+        <div className="brand_box"></div>
+        <div className="text_box">
+          <h1 className="heading_primary">
+            <span className="heading_primary_main">
+              Welcome to the world of ReactJS
+            </span>
+            <span>Love with JavaScript</span>
+          </h1>
+          <a href="#" className="btn btn_white btn_animated">
+            Thapa Technical
+          </a>
+        </div>
+      </handler>
     </>
   );
 };
