b94b5252d833b5efdf1c7d78b3123a407f79bf8b
abirahmed56 <abirahmedsohan6@gmail.com>
2023-02-15 17:24:36 +0600

    change js to jsx

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,5 +1,31 @@
-const Error = ()=>{
-    return {
-        
-    }
+import React from "react";
+import Link from "next/link";
+const ErrorPage = ()=>{
+    return (
+        <>
+            <nav>
+                <ul className="menu-bar">
+                <Link href="blogs/Home">
+                    Home
+                </Link>
+                <Link href="blogs/about">
+                    About
+                </Link>
+                <Link href="blogs/contact">
+                    Contact
+                </Link>
+                <Link href="blogs">
+                    Blog
+                </Link>
+                <Link href="blogs/product">
+                    Product
+                </Link>
+                </ul>
+            </nav>
+
+            <h1>404 ERRORS</h1>
+
+        </>
+    )
 }
+export default ErrorPage;
